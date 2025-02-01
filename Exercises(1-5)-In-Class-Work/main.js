2ec27b2e82bd57efb3b67b6fc94eeee4d0e8a5de
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-02-01 17:22:01 -0600

    did all my exercises!

---
+++ Exercises(1-5)-In-Class-Work/main.js
@@ -0,0 +1,66 @@
+                // ** In-Class Exercises For Thursday, 01-30-25 **
+
+
+        // 1: CREATE A TO-DO LIST
+
+// const todoList = ["Wake Up", "Eat", "Shower"];
+// for (let i = 0; i < todoList.length; i++) {
+//         console.log(todoList[i]);
+// }
+// todoList.push("Put On Clothes", "Feed Cats");
+// console.log(todoList);
+// todoList.shift();
+// todoList.shift();
+// console.log(todoList);
+
+
+// ---
+
+
+        // 2: COUNT EVEN NUMBERS
+
+// const numbers = [102, 21, 56, 67, -1, 2, 8];
+// let evens = [];
+// for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 === 0) evens.push(numbers[i]);
+//         }
+// console.log(evens.length);
+
+
+// ---
+
+
+        // 3: REVERSE AN ARRAY (I did a music scale woot!)
+
+// const fMajorScale = ["F", "G", "A", "B-Flat", "C", "D", "E", "F"]
+// console.log(`F-Major Scale Ascending: ${fMajorScale}`);
+// console.log(`F-Major Scale Descending: ${fMajorScale.reverse()}`);
+
+
+// ---
+
+
+        // 4: FIND THE LARGEST NUMBER
+// function biggestNumber () {     
+//         const numberArray = [1, -2, 5, -4, -100, -1200]
+//         let maxNumber = 0
+//         for (let i = 0; i < numberArray.length; i++) {
+//         if (numberArray[i] > maxNumber)
+//                 maxNumber = numberArray[i];
+//           }
+//         console.log(maxNumber);
+// }
+// biggestNumber();
+
+
+// ---
+
+
+        // 5: DYNAMIC GREETING
+// function dynamicGreeting() {
+//         const greetUsers = ["Steffan", "Foot", "Strawberry", "Flea Flea"]
+//         for (let i = 0; i < greetUsers.length; i++) {
+//                 console.log(`Welcome ${greetUsers[i]}!!`)
+//         }
+// }
+// dynamicGreeting();
